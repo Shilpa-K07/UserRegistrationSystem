33f64f3a8c15d875a1ec6bffa0a361dce2de487e
shilpa <shilpa07udupi@gmail.com>
2020-11-02 17:55:16 +0530

    feat: add validation for first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+	Scanner scanner = new Scanner(System.in);	
+
 	public static final String NAMEPATTERN = "[A-Z][A-Za-z]{2,}";
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration !");
+
+		UserRegistration userRegistration = new UserRegistration();
+		userRegistration.readUserInput();
+	}
+
+	private void readUserInput() {
+		System.out.println("Enter first name ");
+		String firstName = scanner.next();
+		validateUserInput(NAMEPATTERN, firstName);
+	}
+
+	private void validateUserInput(String patttern, String input) {
+		if(Pattern.matches(patttern, input)) {
+			System.out.println("Valid Input");
+		}
+		else {
+			System.out.println("Invalid Input");
+		}
 	}
 }
