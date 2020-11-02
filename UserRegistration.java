9b462f6c893d7ebf627d8265e1b07041ec0fcff9
shilpa <shilpa07udupi@gmail.com>
2020-11-02 21:49:42 +0530

    feat: add exception to test cases

--- UserRegistration.java
+++ UserRegistration.java
@@ -12,19 +12,47 @@ public class UserRegistration {
 	public static final String PASSWORDPATTERN = "^(?=.{8,})(?=.*[a-z])(?=.*[0-9])"
 											+ "(?=.*[A-Z])(?=.*\\W){1}.*$";
 	
-	public boolean validatetName(String pattern) {
-		return (Pattern.matches(NAMEPATTERN, pattern));
+	public boolean validatetName(String pattern) throws UserRegistrationException {
+		try {
+			boolean result = Pattern.matches(NAMEPATTERN, pattern);
+			if(!result)
+				throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_INVALID, "Please give valid name");
+			return result;
+		}catch(NullPointerException exception) {
+			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "Name should not be null");
+		}
 	}
 
-	public boolean validateEmail(String pattern) {
-		return (Pattern.matches(EMAILPATTERN, pattern));
+	public boolean validateEmail(String pattern) throws UserRegistrationException {
+		try {
+			boolean result = Pattern.matches(EMAILPATTERN, pattern);
+			if(!result)
+				throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_INVALID, "Please give valid email");
+			return result;
+		}catch(NullPointerException exception) {
+			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "Email should not be null");
+		}
 	}
 	
-	public boolean validateMobileNumber(String pattern) {
-		return (Pattern.matches(MOBILENUMBERPATTERN, pattern));
+	public boolean validateMobileNumber(String pattern) throws UserRegistrationException {
+		try {
+			boolean result = Pattern.matches(MOBILENUMBERPATTERN, pattern);
+			if(!result)
+				throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_INVALID, "Please give valid mobile number");
+			return result;
+		}catch(NullPointerException exception) {
+			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "Mobile number should not be null");
+		}
 	}
 	
-	public boolean validatePassword(String pattern) {
-		return (Pattern.matches(PASSWORDPATTERN, pattern));
+	public boolean validatePassword(String pattern) throws UserRegistrationException {
+		try {
+			boolean result = Pattern.matches(PASSWORDPATTERN, pattern);
+			if(!result)
+				throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_INVALID, "Please give valid passsword");
+			return result;
+		}catch(NullPointerException exception) {
+			throw new UserRegistrationException(UserRegistrationException.ExceptionType.ENTERED_NULL, "Password should not be null");
+		}
 	}
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -1,35 +1,56 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class UserRegistrationTest {
 	
 	@Test
 	public void givenFirstName_WhenMatchesPattern_ShouldReturnTrue() {
 		UserRegistration userRegistartion = new UserRegistration();
-		Assert.assertTrue(userRegistartion.validatetName("Shilpa"));
+		try {
+			Assert.assertTrue(userRegistartion.validatetName("Si"));
+		} catch (UserRegistrationException exception) {
+			exception.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void givenLastName_WhenMatchesPattern_ShouldReturnTrue() {
 		UserRegistration userRegistration = new UserRegistration();
-		Assert.assertTrue(userRegistration.validatetName("Kundapur"));
+		try {
+			Assert.assertTrue(userRegistration.validatetName("Kundapur"));
+		} catch (UserRegistrationException exception) {
+			exception.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void givenEmail_WhenMatchesPattern_ShouldReturnTrue() {
 		UserRegistration userRegistration = new UserRegistration();
-		Assert.assertTrue(userRegistration.validateEmail("shilpaudupi@gmail.com"));
+		try {
+			Assert.assertTrue(userRegistration.validateEmail("shilpaudupi@gmail.com"));
+		} catch (UserRegistrationException exception) {
+			exception.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void givenMobileNumber_WhenMatchesPattern_ShouldReturnTrue() {
 		UserRegistration userRegistration = new UserRegistration();
-		Assert.assertTrue(userRegistration.validateMobileNumber("91 7767978765"));
+		try {
+			Assert.assertTrue(userRegistration.validateMobileNumber("91 7767978765"));
+		} catch (UserRegistrationException exception) {
+			exception.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void givenPassword_WhenMatchesPattern_ShouldReturnTrue() {
 		UserRegistration userRegistration = new UserRegistration();
-		Assert.assertTrue(userRegistration.validatePassword("As#w23dzd"));
+		try {
+			Assert.assertTrue(userRegistration.validatePassword("As#w23dzd"));
+		} catch (UserRegistrationException exception) {
+			exception.printStackTrace();
+		}
 	}
 }
