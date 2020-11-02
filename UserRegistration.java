66366b2a7ae192781dfe9e86225a28a1fa6794c6
shilpa <shilpa07udupi@gmail.com>
2020-11-02 17:24:36 +0530

    feat: add welcome message

---
+++ UserRegistration.java
@@ -0,0 +1,7 @@
+public class UserRegistration {
+	public static final String NAMEPATTERN = "[A-Z][A-Za-z]{2,}";
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration !");
+	}
+}
