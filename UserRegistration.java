66779634612f5925bb01f6aa01dfb0bd90a450e4
shilpa <shilpa07udupi@gmail.com>
2020-11-02 17:41:37 +0530

    refactor: refactoring to remove unwanted code

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,6 +1,5 @@
 public class UserRegistration {
-	public static final String NAMEPATTERN = "[A-Z][A-Za-z]{2,}";
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration !");
 	}
